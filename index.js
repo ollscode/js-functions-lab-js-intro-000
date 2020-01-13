4c9c70dfadc923840f8e0bb023ccd3a0291cbb5d
Oscar <mundonatural@gmail.com>
2020-01-13 14:40:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ function happyHolidaysTo(nombre) {
   return `Happy holidays, ${nombre}!`;
 }
 
+function happyHolidayTo(dia, nombre) {
+  return `Happy ${dia}, ${nombre}!`;
+}
+
 // write your code below!
